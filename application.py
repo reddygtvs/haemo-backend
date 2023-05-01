5b1bfb2c8eb358762163831a26e36c8e958dddd6
Tushar <reddygtvs@gmail.com>
2023-05-01 10:49:10 +0530

    changed files for production build

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
-from flask import Flask, request, jsonify, send_from_directory
-from flask_cors import CORS
-from flask_cors import cross_origin
+from flask import Flask, request, jsonify, send_from_directory, redirect
+#from flask_cors import CORS
+#from flask_cors import cross_origin
 from keras.models import load_model
 from PIL import Image
 import tensorflow as tf
@@ -8,8 +8,8 @@ import os
 import io
 import numpy as np
 
-app = Flask(__name__, static_folder='build')
-CORS(app)
+application = app = Flask(__name__, static_folder='build')
+#CORS(app)
 
 # Load the h5 model
 model = load_model("models/binaryClassifierV2.h5")
@@ -21,20 +21,20 @@ def preprocess_image(img):
     img = tf.image.resize(img, (256, 256))
     
     return img
+#@application.route("/")
 
-#@app.route("/")
 #def hello_world():
     #return "<p>Hello, World!</p>"
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
+@application.route("/", defaults={"path": ""})
+@application.route("/<path:path>")
 def serve(path):
-    if path != "" and os.path.exists(app.static_folder + "/" + path):
-        return send_from_directory(app.static_folder, path)
+    if path != "" and os.path.exists(application.static_folder + "/" + path):
+        return send_from_directory(application.static_folder, path)
     else:
-        return send_from_directory(app.static_folder, "index.html")
+        return send_from_directory(application.static_folder, "index.html")
 
 
-@app.route("/predict", methods=["POST"])
+@application.route("/predict", methods=["POST"])
 def predict():
     img = request.files["image"]
     
@@ -54,7 +54,7 @@ def predict():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route("/classify", methods=["POST"])
+@application.route("/classify", methods=["POST"])
 def classification():
     img = request.files["image"]
     
@@ -80,4 +80,4 @@ def classification():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    application.run(debug=True)
